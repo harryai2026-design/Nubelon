7e85a1a925d882331ca2f068655743db37403a01
Nubelon <harryai2026@gmail.com>
2026-09-04 20:31:26 +0800

    ci: add GitHub Pages preview deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://nubelon.com',
+  base: '/Nubelon',
   i18n: { defaultLocale: 'en', locales: ['en', 'ar', 'zh'], routing: { prefixDefaultLocale: true } }
 });
